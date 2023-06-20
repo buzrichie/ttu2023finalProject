c564798a63083210f56e06d200fe63b28b8f5ffc
Richmond Nyarko <richmondnyarko123@gmail.com>
2023-06-20 09:23:51 +0100

    edited the server file by importing and setting up the routes

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
 require("dotenv").config();
 
 const express = require("express");
-const admissionRoute = require("./routes/admissionRoutes");
+
+//Imported Route files
+const admissionRoutes = require("./routes/admissionRoutes");
+const assessmentRoutes = require("./routes/assessmentRoutes");
+const attendanceRoutes = require("./routes/attendanceRoutes");
+const financeRoutes = require("./routes/financeRoutes");
+const parentGuardianRoutes = require("./routes/parentGuardianRoutes");
+const schoolRoutes = require("./routes/schoolRoutes");
+const studentRoutes = require("./routes/studentRoutes");
+const subjectRoutes = require("./routes/subjectRoutes");
+const teacherRoutes = require("./routes/teacherRoutes");
+
 //Express App
 const app = express();
-
-const db = [];
 
 //Initiatiate Communications Between JSON files
 app.use(express.urlencoded({ extended: true }));
@@ -17,7 +26,15 @@ app.use((req, res, next) => {
 });
 
 //Routes
-app.use("/admission", admissionRoute);
+app.use("/api/admission", admissionRoutes);
+app.use("/api/assessment", assessmentRoutes);
+app.use("/api/attendance", attendanceRoutes);
+app.use("/api/finance", financeRoutes);
+app.use("/api/parent-Guardian", parentGuardianRoutes);
+app.use("/api/school", schoolRoutes);
+app.use("/api/student", studentRoutes);
+app.use("/api/subject", subjectRoutes);
+app.use("/api/teacher", teacherRoutes);
 
 //Listen for request
 app.listen(process.env.PORT, () =>
